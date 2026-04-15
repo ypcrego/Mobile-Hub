2a88d2a5bcdee934b69a8e50a01aeee610f4b12f
Yves Pablo Costa Rêgo <yves.costa.706@ufrn.edu.br>
2026-04-14 23:42:46 -0300

    feat: add main hub theme
    - fix: fixed back button in landscape mode (calculator)

--- app/src/main/java/com/example/mobilehub/MainActivity.kt
+++ app/src/main/java/com/example/mobilehub/MainActivity.kt
@@ -1,31 +1,50 @@
 package com.example.mobilehub
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnTema: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnCalculadora = findViewById<Button>(R.id.btn_calculadora)
-        val btnPato = findViewById<Button>(R.id.btn_pato)
-        val btnPomodoro = findViewById<Button>(R.id.btnPomodoro)
+        btnTema = findViewById(R.id.btnTema)
 
-        btnCalculadora.setOnClickListener {
-            val intent = Intent(this, CalculadoraActivity::class.java)
-            startActivity(intent)
+        val cardPato = findViewById<CardView>(R.id.card_pato)
+        val cardCalculadora = findViewById<CardView>(R.id.card_calculadora)
+        val cardPomodoro = findViewById<CardView>(R.id.card_pomodoro)
+
+
+        btnTema.setOnClickListener {
+            val isNightMode = (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+
+            if (isNightMode) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
         }
 
-        btnPato.setOnClickListener {
+        cardPato.setOnClickListener {
             val intent = Intent(this, PatoActivity::class.java)
             startActivity(intent)
         }
 
-        btnPomodoro.setOnClickListener {
+        cardCalculadora.setOnClickListener {
+            val intent = Intent(this, CalculadoraActivity::class.java)
+            startActivity(intent)
+        }
+
+        cardPomodoro.setOnClickListener {
             val intent = Intent(this, PomodoroActivity::class.java)
             startActivity(intent)
         }
